1ebe908da12725b314849990fed158df0814aa95
ejerry27 <ejerry@andrew.cmu.edu>
2024-12-04 11:43:58 -0500

    add

--- code-finalproject/project2.js
+++ code-finalproject/project2.js
@@ -1,10 +1,15 @@
+
+// animation for the cars that will slide in 
 document.addEventListener('DOMContentLoaded',()=>{
     anime({
         targets:'.card',
         translateY:[50,0],
         opacity:[0,1],
+        // delaying the time
         delay:anime.stagger(500),
+        // type of animation
         easing:'easeOutQuad',
+        // how long it will be there
         duration:800,
     });
 });
